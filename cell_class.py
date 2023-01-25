10e2021576eb60cc81ed00827f18d7c215a0b938
mostafafrm <48805317+mostafafrm@users.noreply.github.com>
2023-01-25 17:24:50 +0330

    final edit.

--- cell_class.py
+++ cell_class.py
@@ -1,6 +1,4 @@
-import statistics
 import sys
-
 from itertools import chain
 
 
@@ -41,102 +39,60 @@ class Cell:
 
         return possible_neighbors_set
 
-
-def check_win_traverse(cell, traversed, head, tail, color):
+def calculate_utility_traverse(cell, traversed, head, tail, color):
     traversed.add(cell)
     if color == 1:  # blue
-        if cell.number % 7 == 0:
-            head = True
-        if cell.number % 7 == 6:
-            tail = True
+        head = min(head, cell.number % 7)
+        tail = max(tail, cell.number % 7)
     else:  # red
-        if cell.number // 7 == 0:
-            head = True
-        if cell.number // 7 == 6:
-            tail = True
+        head = min(head, cell.number // 7)
+        tail = max(tail, cell.number // 7)
     for adj_cell in cell.adjacent_cells:
         if adj_cell not in traversed:
-            head, tail = check_win_traverse(adj_cell, traversed, head, tail, color)
+            head, tail = calculate_utility_traverse(adj_cell, traversed, head, tail, color)
     return head, tail
-
-
-def check_win(player_set, color):
-    traversed = set()
-    for cell in player_set:
-        head = False
-        tail = False
-        if cell not in traversed:
-            head, tail = check_win_traverse(cell, traversed, head, tail, color)
-            if head and tail:
-                return True
-    return False
-
-
-def calculate_utility_traverse(cell, traversed, total_thread_length, horizontal_indices, vertical_indices):
-    traversed.add(cell)
-    horizontal_indices.append(cell.number % 7)
-    vertical_indices.append(cell.number // 7)
-    # if len(cell.adjacent_cells - traversed) != 0:
-    #     total_thread_length += 1
-    for adj_cell in cell.adjacent_cells:
-        if adj_cell not in traversed:
-            total_thread_length += 1
-            total_thread_length = calculate_utility_traverse(adj_cell, traversed, total_thread_length,
-                                                             horizontal_indices, vertical_indices)
-    return total_thread_length
-
 
 def calculate_utility(player_set, color):
     traversed = set()
-    total_thread_length = 0
-    horizontal_indices = list()
-    vertical_indices = list()
+    longest_thread = 0  # longest head-to-tail difference
     for cell in player_set:
         if cell not in traversed:
-            total_thread_length = calculate_utility_traverse(cell, traversed, total_thread_length, horizontal_indices,
-                                                             vertical_indices)
-    horizontal_indices_pstdev = statistics.pstdev(horizontal_indices)  # pstdev: population standard deviation
-    vertical_indices_pstdev = statistics.pstdev(vertical_indices)
-    if horizontal_indices_pstdev == 0:
-        horizontal_indices_pstdev += 1 / sys.maxsize
-    if vertical_indices_pstdev == 0:
-        vertical_indices_pstdev += 1 / sys.maxsize
-    if color == 1:  # blue
-        utility = pow(horizontal_indices_pstdev / vertical_indices_pstdev, total_thread_length)
-    else:  # red
-        utility = pow(vertical_indices_pstdev / horizontal_indices_pstdev, total_thread_length)
-    return utility
-
+            if color == 1:
+                head = tail = cell.number % 7
+            else:
+                head = tail = cell.number // 7
+            head, tail = calculate_utility_traverse(cell, traversed, head, tail, color)
+            longest_thread = max(longest_thread, tail-head)
+    return longest_thread
 
 def color_cell(player_set: set[Cell], white_set: set[Cell], white_cell: Cell):
     white_set -= {white_cell}
     player_set |= {white_cell}
     for fellow_cell in player_set:
-        if fellow_cell.is_valid_neighbor(white_cell):
-            fellow_cell.adjacent_cells.add(white_cell)
         if white_cell.is_valid_neighbor(fellow_cell):
+            fellow_cell.adjacent_cells.add(white_cell)
             white_cell.adjacent_cells.add(fellow_cell)
 
 
-def uncolor_cell(player_set: set[Cell], white_set: set[Cell], white_cell: Cell):
+def uncolor_cell(player_set, white_set, white_cell):
     player_set -= {white_cell}
     white_set |= {white_cell}
     for fellow_cell in player_set:
-        if fellow_cell.is_valid_neighbor(white_cell):
-            fellow_cell.adjacent_cells.remove(white_cell)
         if white_cell.is_valid_neighbor(fellow_cell):
+            fellow_cell.adjacent_cells.remove(white_cell)
             white_cell.adjacent_cells.remove(fellow_cell)
 
 
-def negamax(player_set: set[Cell], opponent_set: set[Cell], white_set: set[Cell], depth: int, alpha: int, beta: int, color: int):
+def negamax(player_set: set[Cell], opponent_set: set[Cell], white_set: set[Cell], depth: int, alpha, beta, color):
     # color: 1 for blue, -1 for red
-    if depth == 0 or len(white_set) == 0 or check_win(opponent_set, -color):
+    utility = calculate_utility(opponent_set, -color)
+    if depth == 0 or len(white_set) == 0 or utility == 6:
         if color == 1:
             return color * (calculate_utility(player_set, color) - calculate_utility(opponent_set, -color)), None
         else:
             return color * (calculate_utility(opponent_set, -color) - calculate_utility(player_set, color)), None
     value = -sys.maxsize
-    move = -1
+    move = list(white_set)[0].number
     for white_cell in white_set.copy():
         color_cell(player_set, white_set, white_cell)
         negamax_result, _ = negamax(opponent_set, player_set, white_set, depth - 1, -beta, -alpha, -color)
@@ -176,7 +132,7 @@ def main():
             print(place_of_move)
             list_of_lines[place_of_move] = "B "
             console(list_of_lines)
-            if check_win(blue_set, 1) and len(blue_set) >= 7:
+            if calculate_utility(blue_set, 1) == 6:
                 print("Blue won")
                 break
             if len(white_set) == 0:
@@ -194,7 +150,7 @@ def main():
             color_cell(red_set, white_set, cell_list[place_of_move])
             list_of_lines[place_of_move] = "R "
             console(list_of_lines)
-            if check_win(red_set, -1) and len(red_set) >= 7:
+            if calculate_utility(red_set, -1) == 6:
                 print("Red won.")
                 break
             if len(white_set) == 0:
@@ -209,7 +165,7 @@ def console(lines: list[str]):
     row_space = 0
     for i in range(0, 9):
         print("\033[31mR   ", end=" ")
-    print("\033[0m", end="")
+    # print("\033[0m", end="")
 
     for i in range(0, 8):
         for numOfRow in range(0, row_space):
